10d1ecd2c99a574523708fe3c287d7035483a724
IsabellaGuo <isabella.guofei@gmail.com>
2020-02-12 21:41:46 -0800

    Port set up

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code away!
+const express = require ("express");
+const server = express();
+
+
+//port
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`\n** Server Running on http://localhost:${port} **\n`);
+});
+
